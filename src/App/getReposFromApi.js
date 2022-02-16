344b29fb10d65d0eaea1d268538838e2fa2ae3c2
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-02-16 20:00:10 +0100

    used axios async to get repositories from api.

--- src/App/getReposFromApi.js
+++ src/App/getReposFromApi.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
-export const getRepositoriesFromApi = () => {
-  axios
-    .get("https://api.github.com/users/MieczyslawFrontDev/repos")
-    .then((response) => response.data);
+export const getRepositoriesFromApi = async () => {
+  const response = await axios.get(
+    "https://api.github.com/users/MieczyslawFrontDev/repos"
+  );
+
+  const repositories = response.data;
+  console.log(repositories);
+  return await repositories;
 };
